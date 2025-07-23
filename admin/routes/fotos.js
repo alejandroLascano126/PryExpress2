4c874e039866287e1183eab407af233eafeaea09
Alejandro Lascano <alejandrolascano126@gmail.com>
2025-07-22 19:14:47 -0500

    Commit de clase

--- admin/routes/fotos.js
+++ admin/routes/fotos.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 
 const Sequelize = require('sequelize');
+const { render } = require('../app');
 const Foto = require('../models').Foto;
 const Etiqueta = require('../models').etiqueta;
 
@@ -23,7 +24,28 @@ router.get('/findAll/json',
                 res.status(400).send(error))
     });
 
+router.get('/findById/json',
+    function (req, res, next) {
+        const id = parseInt(req.query.id) || 0;
+
+        Foto.findOne({
+            where: { id: id },
+            attributes: { exclude: ["updatedAt"] },
+            include: [{
+                model: Etiqueta,
+                attributes: ['texto'],
+                through: { attributes: [] }
+            }]
+        })
+            .then(fotos => {
+                res.json(fotos);
+            })
+            .catch(error =>
+                res.status(400).send(error))
+    });
+
 router.get('/findAll/view', function (req, res, next) {
+
     Foto.findAll({
         attributes: { exclude: ["updatedAt"] },
         include: [{
@@ -32,10 +54,47 @@ router.get('/findAll/view', function (req, res, next) {
             through: { attributes: [] }
         }]
     })
-    .then(fotos => {
-        res.render('fotos', { title: 'Fotos', arrFotos: fotos });
-    })
-    .catch(error => res.status(400).send(error));
+        .then(fotos => {
+            res.render('fotos', { title: 'Fotos', arrFotos: fotos });
+        })
+        .catch(error => res.status(400).send(error));
+});
+
+
+router.get('/findById/view', function (req, res, next) {
+    const id = parseInt(req.query.id) || 0;
+
+    if (id > 0) {
+        Foto.findOne({
+            where: { id: id },
+            attributes: { exclude: ["updatedAt"] },
+            include: [{
+                model: Etiqueta,
+                attributes: ['texto'],
+                through: { attributes: [] }
+            }]
+        })
+            .then(foto => {
+                res.render('fotos', { title: 'Fotos', arrFotos: foto ? [foto] : [] });
+            })
+            .catch(error => {
+                console.error(error);
+                res.status(400).send(error.message);
+            });
+    } else {
+        Foto.findAll({
+            attributes: { exclude: ["updatedAt"] },
+            include: [{
+                model: Etiqueta,
+                attributes: ['texto'],
+                through: { attributes: [] }
+            }]
+        })
+            .then(fotos => {
+                res.render('fotos', { title: 'Fotos', arrFotos: fotos });
+            })
+            .catch(error => res.status(400).send(error));
+    }
 });
 
 module.exports = router;
